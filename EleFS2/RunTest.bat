a5dbeaa90a6244da4dab826939430af5ae6a819a
Martin Piper <martin.piper@gmail.com>
2021-07-15 21:24:39 +0800

    Update EleFS2 test to include more file movement code

--- EleFS2/RunTest.bat
+++ EleFS2/RunTest.bat
@@ -1,8 +1,22 @@
 mkdir C:\temp\dokanempty
 
 rmdir /s /q m:\temp
-rem robocopy c:\ReplicaNet\ReplicaNetPublic\Includes m:\temp\ /s
-robocopy c:\ReplicaNet\ReplicaNetPublic\Includes\RNLobby m:\temp\ /s
+robocopy c:\ReplicaNet\ReplicaNetPublic\Includes m:\temp\ /s
 
-rem "C:\Program Files\Git\usr\bin\diff.exe" -r -q c:\ReplicaNet\ReplicaNetPublic\Includes m:\temp\
-"C:\Program Files\Git\usr\bin\diff.exe" -r -q c:\ReplicaNet\ReplicaNetPublic\Includes\RNLobby m:\temp\
+"C:\Program Files\Git\usr\bin\diff.exe" -r -q c:\ReplicaNet\ReplicaNetPublic\Includes m:\temp\
+
+echo Rename the files
+rename m:\temp\RNLobby RNLobby2
+rename m:\temp\RNLog RNLog2
+rename m:\temp\RNLobby2\Inc\_SrvCommon.cpp _SrvCommonNew.cpp
+rename m:\temp\RNLog2\Inc\Log.h LogNew.h
+
+echo Then restore the files
+rename m:\temp\RNLobby2\Inc\_SrvCommonNew.cpp _SrvCommon.cpp
+rename m:\temp\RNLog2\Inc\LogNew.h Log.h
+
+rename m:\temp\RNLobby2 RNLobby
+rename m:\temp\RNLog2 RNLog
+
+echo Then check the data again
+"C:\Program Files\Git\usr\bin\diff.exe" -r -q c:\ReplicaNet\ReplicaNetPublic\Includes m:\temp\
